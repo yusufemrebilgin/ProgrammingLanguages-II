af841bba88b242cf4c5335cd4c92cde53cb3feb2
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2023-03-11 16:58:36 +0300

    shortestPath with recursive

---
+++ shortestPath/README.md
@@ -0,0 +1,5 @@
+# Recursive Shortest Path Homework
+
+*Let MxN be a matrix of 0 and 1 values. Write the C program that finds the shortest path from 0x0 to MxN point. 1 values in the matrix represents the path while 0 represents the wall. So you cannot go to the coordinate or position with 0 value. You can move in four directions in total. Up, down, left and right. Below is an example path.*
+
+![screenshot](https://user-images.githubusercontent.com/87023953/224488002-4460aa73-4783-4f81-9ea0-44f6dedb3a81.png)
---
+++ shortestPath/findShortestPath.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#define M 6
+#define N 12
+int path[M][N] ={};
+void findPath(int,int,int[][N]);
+void printPath(void);
+int main(){
+	
+	int i, j;
+	int array[M][N] = {		{1,0,1,1,1,0,0,1,0,1,1,0},
+							{1,0,1,1,1,1,1,0,1,0,0,0},
+							{1,1,1,1,1,1,1,0,1,0,1,0},
+							{0,1,0,1,0,1,0,1,1,1,1,1},
+							{0,1,0,1,1,1,1,1,0,0,0,1},
+							{1,1,1,0,1,1,1,1,1,1,1,1}		};
+	
+	printf("MxN (%dx%d):\n\n",M,N);	
+	for(i=0 ; i<M ; i++){
+		for(j=0 ; j<N ; j++)
+			printf("%d ", array[i][j]);
+		printf("\n");
+	}
+	findPath(0,0,array);	
+	printPath();
+	
+	return 0;
+}
+
+void findPath(int i, int j, int arr[][N]){
+	
+	static int minCost, maxCost=M*N;
+	arr[i][j]=2;
+	minCost++;
+	
+	(arr[i+1][j]==1 && arr[i+1][j]!=2) ? findPath(i+1,j,arr) : 0 ;
+	(arr[i][j+1]==1 && arr[i][j+1]!=2) ? findPath(i,j+1,arr) : 0 ;
+	(arr[i-1][j]==1 && arr[i-1][j]!=2) ? findPath(i-1,j,arr) : 0 ;
+	(arr[i][j-1]==1 && arr[i][j-1]!=2) ? findPath(i,j-1,arr) : 0 ;
+	
+	if( minCost<maxCost && (i==M-1 && j==N-1) ){
+		maxCost=minCost;
+		for(i=0;i<M;i++)
+			for(j=0;j<N;j++)
+				path[i][j]=arr[i][j];
+		minCost=0;
+	}
+	minCost--;
+	arr[i][j]=1;
+}
+void printPath(){
+	
+	int i, j;
+	printf("\n\nSHORTEST PATH:\n\n");
+	for(i=0 ; i<M ; i++){
+		for(j=0 ; j<N ; j++)
+			(path[i][j]==2) ? printf("%c ",254):printf("%d ",path[i][j]);
+		printf("\n");	
+	}
+}
