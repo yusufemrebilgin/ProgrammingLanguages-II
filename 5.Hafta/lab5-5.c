d80c35ef1e6a428b163ee233112e297a4629efb4
Yusuf Emre Bilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2023-04-14 22:11:08 +0300

    Update lab5-5.c

--- 5.Hafta/lab5-5.c
+++ 5.Hafta/lab5-5.c
@@ -29,7 +29,7 @@ void swapElements(int *arr){
     int i, temp;
     for(i=0 ; i<N/2 ; i++){
         temp = *(arr+i);
-        *(arr+i) = *(arr+N-1+i);
+        *(arr+i) = *(arr+N-1-i);
         *(arr+N-1-i) = temp;
     }
 }
